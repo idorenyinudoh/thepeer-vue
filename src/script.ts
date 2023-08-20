813cec7f72aa592b83e9b76bcca9b12bda27d127
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2023-08-20 22:55:04 +0100

    feat: create composable to load thepeer if it doesn't already exist

---
+++ src/script.ts
@@ -0,0 +1,57 @@
+import { ref, onMounted } from 'vue'
+
+type StateType = {
+  loaded: boolean,
+  error: boolean
+}
+
+const thepeerJS = 'https://cdn.thepeer.co/v1/chain.js'
+
+export default function useScript() {
+  const state = ref<StateType>({
+    loaded: false,
+    error: false
+  })
+
+  const onScriptLoad = () => {
+    state.value = {
+      loaded: true,
+      error: false
+    }
+  }
+
+  const onScriptError = () => {
+    state.value = {
+      loaded: true,
+      error: true
+    }
+  }
+
+  onMounted(() => {
+    const scriptElement = document.getElementById('thepeer_script')
+    const scriptSrc = scriptElement && scriptElement.getAttribute('src')
+
+    if (scriptSrc) {
+      return onScriptLoad()
+    }
+
+    const script = document.createElement('script')
+    script.id = 'thepeer_script'
+    script.src = thepeerJS
+    script.async = true
+
+    script.addEventListener('load', onScriptLoad)
+    script.addEventListener('complete', onScriptLoad)
+    script.addEventListener('error', onScriptError)
+    
+    document.body.appendChild(script)
+    
+    return () => {
+      script.removeEventListener('load', onScriptLoad)
+      script.removeEventListener('complete', onScriptLoad)
+      script.removeEventListener('error', onScriptError)
+    }
+  })
+
+  return [ state.value.loaded, state.value.error ]
+}
