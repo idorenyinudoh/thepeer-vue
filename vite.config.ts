a627fc3c02b6981f2107a79273b9f7d9c110c950
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2023-08-21 16:25:01 +0100

    build: configure vite for package build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/index.ts'),
+      name: 'Thepeer',
+      fileName: 'thepeer-vue',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
 })
